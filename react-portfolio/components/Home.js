6c7225cfc8a054aa5c7401e6d0368c0856faa75d
Tristan Maltizo <maltizo2@uic.edu>
2023-04-13 09:56:43 -0500

    test mobile

--- react-portfolio/components/Home.js
+++ react-portfolio/components/Home.js
@@ -75,7 +75,7 @@ export default function Home() {
 
           {/* home */}
           <section id="home" className="text-center pb-10 pr-10 pl-10 mt-35 mb-40">
-            <div className="mx-auto bg-gradient-to-b from-purple-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 mb-5 md:h-96 md:w-96">
+            <div className="mx-auto bg-gradient-to-b from-purple-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 mb-5">
                 <Image src={portrait} alt="Picture of the author" />
             </div>
             <h2 className="text-4xl py-2 text-purple-800 font-medium dark:text-purple-500 md:text-5xl">Hello! <span className= "dark:text-purple-200 text-purple-400">I'm Tristan.</span> 👋</h2>
