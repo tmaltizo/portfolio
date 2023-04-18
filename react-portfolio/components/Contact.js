49ee771f170ed964c76a59c178bfc1a8bef08df3
Tristan Maltizo <maltizo2@uic.edu>
2023-04-18 17:10:06 -0500

    finished emailing stuff

--- react-portfolio/components/Contact.js
+++ react-portfolio/components/Contact.js
@@ -69,12 +69,12 @@ export default function Contact() {
           className="border rounded-md p-2 w-full text-purple-900"
           rows="5"
         />
-        <button type="submit" className="bg-purple-800 dark:bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-500 dark:hover:bg-purple-400">
-          Send
-        </button>
         {isSent && (
           <p className="text-green-400 mt-2">Email sent successfully. I'll try to reply as soon as possible!</p>
         )}
+        <button type="submit" className="bg-purple-800 dark:bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-500 dark:hover:bg-purple-400">
+          Send
+        </button>
       </form>
     )
 }
