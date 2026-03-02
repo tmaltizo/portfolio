17c0b7291c59640416675525f91113f646d78e52
tmaltizo <tristan.maltizo@gmail.com>
2026-03-01 23:28:47 -0600

    Move page tests out of pages directory to avoid build errors

---
+++ tests/pages/writing.test.js
@@ -0,0 +1,29 @@
+/**
+ * T014 – Writing page heading test
+ *
+ * Verifies that the /writing page renders a level-1 heading containing
+ * "Writing".
+ * This test WILL FAIL until pages/writing.js is created (TDD red phase).
+ */
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import WritingPage from '../../pages/writing'
+
+describe('Writing page', () => {
+  it('renders an <h1> element', () => {
+    render(<WritingPage />)
+    expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument()
+  })
+
+  it('h1 contains the text "Writing"', () => {
+    render(<WritingPage />)
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(/writing/i)
+  })
+
+  it('renders some explanatory text describing the blog section', () => {
+    render(<WritingPage />)
+    const paragraphs = screen.getAllByRole('paragraph')
+    expect(paragraphs.length).toBeGreaterThanOrEqual(1)
+  })
+})
