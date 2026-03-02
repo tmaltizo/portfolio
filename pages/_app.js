230b6f2174880a66790c88264acf0f82bb5f8700
tmaltizo <tristan.maltizo@gmail.com>
2026-03-02 11:18:54 -0600

    matched global background with light/dark modes

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,24 @@ import NavBar from '@/components/NavBar'
 import Sidebar from '@/components/Sidebar'
 import { DarkModeProvider, useDarkMode } from '@/contexts/DarkModeContext'
 
+import { useEffect } from 'react'
+
 function AppShell({ Component, pageProps }) {
   const { isDark } = useDarkMode()
+
+  useEffect(() => {
+    const html = document.documentElement
+    if (isDark) html.classList.add('dark')
+    else html.classList.remove('dark')
+  }, [isDark])
+
   return (
-    <div className={isDark ? 'dark' : ''}>
+    <> {/* no wrapper div needed for dark class now */}
       <NavBar />
       <Sidebar />
       <Component {...pageProps} />
       <Analytics />
-    </div>
+    </>
   )
 }
 
