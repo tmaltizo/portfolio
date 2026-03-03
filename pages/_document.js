681bb096f007e02b75b2cb0717cc066960ef6812
tmaltizo <tristan.maltizo@gmail.com>
2026-03-03 00:20:12 -0600

    Add viewport meta tag for mobile scaling

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* mobile viewport, prevents automatic zooming */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
